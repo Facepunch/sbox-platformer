a30ede7f48806de3005893073593f59966a7cc44
bakscratch <lt_8@hotmail.co.uk>
2022-03-08 14:44:31 +0000

    Renamed fall trigger to kill trigger

---
+++ code/hammer/KillTrigger.cs
@@ -0,0 +1,20 @@
+﻿using Sandbox;
+using Hammer;
+
+[Library( "plat_trigger_kill", Description = "Kills the player." )]
+[Hammer.AutoApplyMaterial( "materials/editor/killtrigger/killtrigger.vmat" )]
+[EntityTool( "Trigger Kill", "Platformer", "Kills the player." )]
+internal partial class KillTrigger : BaseTrigger
+{
+
+	public override void StartTouch( Entity other )
+	{
+		base.StartTouch( other );
+
+		if ( !other.IsServer ) return;
+		if ( other is not PlatformerPawn pl ) return;
+
+		Game.Current.DoPlayerSuicide( pl.Client );
+	}
+
+}
