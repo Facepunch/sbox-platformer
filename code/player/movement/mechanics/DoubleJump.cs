0dfa218c93ead6960b13aaca525201c3527e844b
Crayz <jacob.ee92@gmail.com>
2022-03-10 18:05:11 -0700

    Add back double jump effect

--- code/player/movement/mechanics/DoubleJump.cs
+++ code/player/movement/mechanics/DoubleJump.cs
@@ -2,12 +2,15 @@
 
 namespace Facepunch.Parkour
 {
-	class DoubleJump : BaseMoveMechanic
+	partial class DoubleJump : BaseMoveMechanic
 	{
 
 		public float DoubleJumpStrength => 320f;
 		public override bool TakesOverControl => false;
 		public override bool AlwaysSimulate => true;
+
+		[Net, Predicted]
+		public int DoubleJumpsRemaining { get; set; }
 
 		private TimeUntil timeUntilCanDoubleJump;
 		private bool justJumped;
@@ -30,11 +33,15 @@ namespace Facepunch.Parkour
 		{
 			base.PreSimulate();
 
-			if ( Input.Pressed( InputButton.Jump ) && ctrl.GroundEntity != null )
+			if( ctrl.GroundEntity != null )
 			{
-				timeUntilCanDoubleJump = .25f;
-				justJumped = true;
-				return;
+				DoubleJumpsRemaining = 1;
+
+				if ( Input.Pressed( InputButton.Jump ) )
+				{
+					timeUntilCanDoubleJump = .25f;
+					justJumped = true;
+				}
 			}
 
 			if ( justJumped ) return;
@@ -42,8 +49,24 @@ namespace Facepunch.Parkour
 			if ( !Input.Released( InputButton.Jump ) ) return;
 			if ( timeUntilCanDoubleJump > 0 ) return;
 			if ( ctrl.GetMechanic<Glide>()?.Gliding ?? false ) return;
+			if ( DoubleJumpsRemaining <= 0 ) return;
 
 			ctrl.Velocity = ctrl.Velocity.WithZ( DoubleJumpStrength );
+			DoubleJumpsRemaining--;
+
+			DoubleJumpEffect();
+		}
+
+		private void DoubleJumpEffect()
+		{
+			if ( !ctrl.Pawn.IsServer ) return;
+
+			using var _ = Prediction.Off();
+
+			ctrl.AddEvent( "jump" );
+
+			Particles.Create( "particles/gameplay/player/doublejump/doublejump.vpcf", ctrl.Pawn );
+			Sound.FromWorld( "player.djump", ctrl.Pawn.Position );
 		}
 
 	}
