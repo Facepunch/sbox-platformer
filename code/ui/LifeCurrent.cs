de9362feaf039159797662539c0b9cd14c02e5f1
bakscratch <lt_8@hotmail.co.uk>
2022-04-05 22:17:00 +0100

    Remove low life indicator

--- code/ui/LifeCurrent.cs
+++ code/ui/LifeCurrent.cs
@@ -18,7 +18,6 @@ namespace Platformer.UI
 		public LifeCurrent()
 		{
 			Image = Add.Image( "ui/hud/citizen/citizen.png", "playerimage" );
-			Number = Add.Label( "", "number" );
 		}
 
 		public override void Tick()
@@ -33,24 +32,6 @@ namespace Platformer.UI
 
 			_life = _life.LerpTo( life, Time.Delta * 0 );
 
-			Number.SetClass( "lifelow", life <= 1 );
-
-			Number.Text = $"{life}";
-
-			if (life <= 1 )
-			{
-				LowHealth();
-			}
-			if (life == 3)
-			{
-				HighHealth();
-			}
-
-		}
-
-		public void LowHealth()
-		{
-			Image.SetTexture( "ui/hud/citizen/citizen_low.png" );
 		}
 		public void HighHealth()
 		{
