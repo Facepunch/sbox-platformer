1f5c857fd450571e066d57e38da052adad916a18
Jake Evans <jacob.ee92@gmail.com>
2022-04-09 18:02:35 -0600

    Carriable prop

---
+++ code/hammer/PropCarriable.cs
@@ -0,0 +1,21 @@
+﻿
+using Sandbox;
+
+namespace Platformer;
+
+[Library("plat_prop_carriable")]
+internal class PropCarriable : Prop, IUse
+{
+
+	public bool IsUsable( Entity user )
+	{
+		return true;
+	}
+
+	public bool OnUse( Entity user )
+	{
+		Log.Error( "Used a bitch" );
+		return true;
+	}
+
+}
