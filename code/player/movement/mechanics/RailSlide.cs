2af0d5ede86815001f386e25008b51ba5dcb965c
bakscratch <lt_8@hotmail.co.uk>
2022-04-15 13:31:14 +0100

    Rail Slide sounds
    https://files.facepunch.com/louie/1b1511b1/2022-04-15%2013-25-20.mp4

--- code/player/movement/mechanics/RailSlide.cs
+++ code/player/movement/mechanics/RailSlide.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using Sandbox;
 
 namespace Platformer.Movement;
-
 internal class RailSlide : BaseMoveMechanic
 {
 
@@ -13,6 +12,9 @@ internal class RailSlide : BaseMoveMechanic
 	private int Node;
 	private float Alpha;
 	private TimeSince TimeSinceJump;
+	private bool IsRailSliding;
+	private bool DoOnce;
+	private Sound railSlideSound;
 
 	public RailSlide( PlatformerController controller ) : base( controller )
 	{
@@ -20,10 +22,12 @@ internal class RailSlide : BaseMoveMechanic
 
 	protected override bool TryActivate()
 	{
+
 		if ( TimeSinceJump < .3f ) return false;
 
 		foreach ( var path in Entity.All.OfType<RailPathEntity>() )
 		{
+
 			if ( path.PathNodes.Count < 2 ) continue;
 
 			var nearestPoint = path.NearestPoint( ctrl.Position, out int node, out float t );
@@ -35,7 +39,7 @@ internal class RailSlide : BaseMoveMechanic
 
 			return true;
 		}
-
+		
 		return false;
 	}
 
@@ -52,15 +56,17 @@ internal class RailSlide : BaseMoveMechanic
 
 		Alpha += Time.Delta;
 
-		if( Alpha >= 1 )
+		if ( Alpha >= 1 )
 		{
 			Alpha = 0;
 			Node++;
 
-			if( Node >= Path.PathNodes.Count - 1 )
+			if ( Node >= Path.PathNodes.Count - 1 )
 			{
 				IsActive = false;
 				Path = null;
+				SlideStopped();
+				DoOnce = false;
 				return;
 			}
 		}
@@ -77,6 +83,9 @@ internal class RailSlide : BaseMoveMechanic
 		ctrl.SetTag( "skidding" );
 		Particles.Create( "particles/gameplay/player/sliding/railsliding.vpcf", ctrl.Pawn );
 
+		SlideEffect();
+		IsRailSliding = true;
+
 		if ( Input.Pressed( InputButton.Jump ) )
 		{
 			TimeSinceJump = 0;
@@ -86,8 +95,32 @@ internal class RailSlide : BaseMoveMechanic
 			// and fix getting grounded immediately so we don't have to set position
 			ctrl.Velocity = ctrl.Velocity.WithZ( 320f );
 			ctrl.Position = ctrl.Position.WithZ( ctrl.Position.z + 10 );
+			DoOnce = false;
+			SlideStopped();
 		}
 	}
 
-}
+	private void SlideEffect()
+	{
 
+		if ( !ctrl.Pawn.IsServer ) return;
+
+		if ( IsRailSliding && !DoOnce )
+		{
+			DoOnce = true;
+
+			using var _ = Prediction.Off();
+			ctrl.Pawn.PlaySound( "rail.slide.start" );
+			railSlideSound.Stop();
+			railSlideSound = ctrl.Pawn.PlaySound( "rail.slide.loop" );
+		//	Sound.FromEntity( "rail.slide.loop", ctrl.Pawn );
+
+
+		}
+	}
+	private void SlideStopped()
+	{
+		railSlideSound.Stop();
+		ctrl.Pawn.PlaySound( "rail.slide.end" );
+	}
+}
