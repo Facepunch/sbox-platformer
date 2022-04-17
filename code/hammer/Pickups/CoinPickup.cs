2ac32577b728b9da5f09c839076e79da887523ba
bakscratch <lt_8@hotmail.co.uk>
2022-04-17 03:46:57 +0100

    Cooler looking coins

--- code/hammer/Pickups/CoinPickup.cs
+++ code/hammer/Pickups/CoinPickup.cs
@@ -1,15 +1,46 @@
 ﻿
 using Hammer;
 using Sandbox;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Platformer;
 
 [Library( "plat_coin", Description = "Coin Pickup" )]
-[Model( Model = "models/citizen_props/coin01.vmdl" )]
+//[Model( Model = "models/gameplay/collect/coin/coin01.vmdl" )]
+[Hammer.EditorModel( "models/gameplay/collect/coin/coin01.vmdl", FixedBounds = true )]
 [Display( Name = "Coin Pickup", GroupName = "Platformer", Description = "Coin Pickup." )]
 internal partial class CoinPickup : BaseCollectible
 {
+	private Particles CoinPart;
+	private ModelEntity CoinModel;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Transmit = TransmitType.Always;
+
+		MoveType = MoveType.Physics;
+		SetupPhysicsFromModel( PhysicsMotionType.Dynamic,true );
+
+		CollisionGroup = CollisionGroup.Trigger;
+		EnableAllCollisions = true;
+		PhysicsEnabled = false;
+		UsePhysicsCollision = false;
+
+		SetModel( "models/gameplay/collect/coin/coin01.vmdl" );
+
+		SetMaterialGroup( new Random().Next( 0, 3 ).ToString() );
+
+	}
+
+	public void SpawnWithPhys()
+	{
+		PhysicsEnabled = true;
+		UsePhysicsCollision = false;
+
+	}
 
 	protected override bool OnCollected( PlatformerPawn pl )
 	{
@@ -29,4 +60,9 @@ internal partial class CoinPickup : BaseCollectible
 		Particles.Create( "particles/gameplay/player/coincollected/coincollected.vpcf", this );
 	}
 
+	[Event.Tick.Server]
+	public void Tick()
+	{
+		Rotation = Rotation.FromYaw( Rotation.Yaw() + -75 * Time.Delta );
+	}
 }
