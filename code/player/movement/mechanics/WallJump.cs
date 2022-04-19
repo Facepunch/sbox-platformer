776f9ea6e4230ddbe4706edff3d9f6aa04e1d6bd
Sam Pavlovic <sam@sampavlovic.com>
2022-04-19 14:27:06 +0300

    Add walljump

---
+++ code/player/movement/mechanics/WallJump.cs
@@ -0,0 +1,140 @@
+
+using Sandbox;
+
+namespace Platformer.Movement
+{
+	partial class WallJump : BaseMoveMechanic
+	{
+        private float WallJumpConnectangle => 0.75f;
+		public float WallJumpStrength => 240f;
+        public float WallJumpKickStrength => 500f;
+        public float WallJumpFriction => 7.0f;
+		public override bool TakesOverControl => true;
+		public override bool AlwaysSimulate => false;
+        public float TimeUntilNextWallJump = Time.Now;
+
+        private Vector3 HitNormal;
+		public WallJump( PlatformerController controller ) : base( controller )
+		{
+		}
+
+        protected override bool TryActivate()
+		{
+            if( ctrl.Pawn == null )
+                return false;
+
+            // Don't bother if we're on the ground
+            if( ctrl.GroundEntity != null )
+                return false;
+
+            // We have to be falling
+            if( ctrl.Velocity.z >= 0 )
+                return false;
+
+            // Wait until we can grab again
+            if( TimeUntilNextWallJump > Time.Now ) 
+                return false;
+
+            // first try just moving to the destination
+            var center = ctrl.Position;
+            center.z += 48;
+            var dest = (center + ( ctrl.Pawn.Rotation.Forward * 48.0f ) );
+
+            var tr = Trace.Ray( center, dest )
+				.Ignore( ctrl.Pawn )
+				.Radius( 8 )
+				.Run();
+
+            //DebugOverlay.Line( center, dest, Color.Green, 5.0f, false );
+
+            if( tr.Hit )
+            {
+                //If we are on a desired angle, then we can grab the wall
+                bool canGrab = tr.Normal.Dot( -ctrl.Pawn.Rotation.Forward ) > 1.0 - WallJumpConnectangle;
+
+                if( canGrab )
+                {
+                    GrabWall( tr );
+                    return true;
+                }
+            }
+
+            return false;
+		}
+
+        public override void Simulate()
+        {   
+            base.Simulate();
+
+            if ( InputActions.Jump.Pressed() )
+            {
+                DoWallJump();
+                return;
+            }
+            
+            // Shitty gravity
+            ctrl.Velocity *= new Vector3(1).WithZ( 1.0f - ( Time.Delta * WallJumpFriction ) );
+            ctrl.Move();
+
+            Particles.Create( "particles/gameplay/player/Doublejump/Doublejump.vpcf" , ctrl.Pawn );
+
+            //If ground entity, then we hit the ground, stop simulating
+            if( ctrl.GroundEntity != null )
+            {
+                Cancel();
+                return;
+            }
+
+            //If no longer holding wall, then we have nothing to hold on, stop simulating
+
+
+        }
+
+        private void GrabWall( TraceResult tr )
+        {
+            HitNormal = tr.Normal;
+            ctrl.Pawn.Rotation = Rotation.FromYaw(0);
+
+            ctrl.Velocity = 0;
+
+            ctrl.Move();
+        }
+
+        private void DoWallJump()
+        {
+            var jumpVec = HitNormal * WallJumpKickStrength;
+
+            TimeUntilNextWallJump = Time.Now + 0.25f;
+
+            WallJumpEffect();
+
+            ctrl.Velocity = ctrl.Velocity.WithZ( WallJumpStrength );
+            ctrl.Velocity += jumpVec;
+            ctrl.Position += ctrl.Velocity * Time.Delta;
+            IsActive = false;
+
+            // Consume our remaining double jumps
+            ctrl.GetMechanic<DoubleJump>().TimeUntilCanDoubleJump += .25f;
+            ctrl.GetMechanic<DoubleJump>().DoubleJumpsRemaining = 0;
+
+        }
+
+        private void Cancel()
+        {
+            IsActive = false;
+        }
+
+		private void WallJumpEffect()
+		{
+			if ( !ctrl.Pawn.IsServer ) return;
+
+			using var _ = Prediction.Off();
+
+			ctrl.AddEvent( "jump" );
+
+			Particles.Create( "particles/gameplay/player/Walljump/Walljump.vpcf", ctrl.Pawn );
+			Sound.FromWorld( "player.djump", ctrl.Pawn.Position );
+		}
+
+	}
+}
