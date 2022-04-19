f3f5dda363e9460f8b87e7b77bdbf6cbbf36a53f
Sam Pavlovic <sam@sampavlovic.com>
2022-04-19 16:07:10 +0300

    Improved waljump https://i.imgur.com/CBaoXjJ.mp4

--- code/player/movement/mechanics/WallJump.cs
+++ code/player/movement/mechanics/WallJump.cs
@@ -35,10 +35,14 @@ namespace Platformer.Movement
             if( TimeUntilNextWallJump > Time.Now ) 
                 return false;
 
+            // We need to have some horizontal speed to grab
+            if( ctrl.Velocity.WithZ(0).Length < 1.0f )
+                return false;
+
             // first try just moving to the destination
             var center = ctrl.Position;
             center.z += 48;
-            var dest = (center + ( ctrl.Pawn.Rotation.Forward * 48.0f ) );
+            var dest = (center + ( ctrl.Pawn.Rotation.Forward * 40.0f ) );
 
             var tr = Trace.Ray( center, dest )
 				.Ignore( ctrl.Pawn )
@@ -71,11 +75,19 @@ namespace Platformer.Movement
                 DoWallJump();
                 return;
             }
+
+            if( InputActions.Duck.Pressed() )
+            {
+                Cancel();
+                return;
+            }
+            
+            ctrl.SetTag( "grabbing_wall" );
             
             // Shitty gravity
             ctrl.Velocity *= new Vector3(1).WithZ( 1.0f - ( Time.Delta * WallJumpFriction ) );
-            ctrl.Move();
-
+            ctrl.Rotation =  Rotation.LookAt( HitNormal * 10.0f, Vector3.Up );
+            
             Particles.Create( "particles/gameplay/player/Doublejump/Doublejump.vpcf" , ctrl.Pawn );
 
             //If ground entity, then we hit the ground, stop simulating
@@ -87,15 +99,16 @@ namespace Platformer.Movement
 
             //If no longer holding wall, then we have nothing to hold on, stop simulating
 
-
+            ctrl.Move();
         }
 
         private void GrabWall( TraceResult tr )
         {
             HitNormal = tr.Normal;
-            ctrl.Pawn.Rotation = Rotation.FromYaw(0);
-
             ctrl.Velocity = 0;
+
+            // Stick to wall
+            ctrl.Position = ( tr.EndPosition + ( HitNormal * 16.0f ) ).WithZ( ctrl.Position.z );
 
             ctrl.Move();
         }
@@ -112,6 +125,9 @@ namespace Platformer.Movement
             ctrl.Velocity += jumpVec;
             ctrl.Position += ctrl.Velocity * Time.Delta;
             IsActive = false;
+
+            ctrl.Pawn.Rotation =  HitNormal.EulerAngles.ToRotation();
+            ctrl.Pawn.EyeRotation = HitNormal.EulerAngles.ToRotation();
 
             // Consume our remaining double jumps
             ctrl.GetMechanic<DoubleJump>().TimeUntilCanDoubleJump += .25f;
