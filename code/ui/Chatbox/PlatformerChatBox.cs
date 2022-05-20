22c402f05106c50af0b24cd9288d800eb7879af3
Jake Evans <jacob.ee92@gmail.com>
2022-05-20 07:52:53 -0600

    Ambiguity error

--- code/ui/Chatbox/PlatformerChatBox.cs
+++ code/ui/Chatbox/PlatformerChatBox.cs
@@ -131,18 +131,3 @@ namespace Platformer.UI
 
 	}
 }
-
-namespace Sandbox.Hooks
-{
-	public static partial class Chat
-	{
-		public static event Action OnOpenChat;
-
-		[ConCmd.Client( "openchat" )]
-		internal static void MessageMode()
-		{
-			OnOpenChat?.Invoke();
-		}
-
-	}
-}
