0dcdd6c69900769da2cd53341bc33b1782376c0e
Jake Evans <jacob.ee92@gmail.com>
2022-06-07 11:16:42 -0600

    Bit of cleanup

--- code/hammer/Gameplay/PlatformerDecay.cs
+++ code/hammer/Gameplay/PlatformerDecay.cs
@@ -24,6 +24,8 @@ public partial class PlatformerDecay : ModelEntity
 	public float DecayTime { get; set; } = 5f;
 	[Net, Property]
 	public float RespawnTime { get; set; } = 5f;
+	[Net]
+	public bool Broken { get; set; }
 
 	private Color DefaultColor;
 	public override void Spawn()
@@ -49,7 +51,6 @@ public partial class PlatformerDecay : ModelEntity
 
 	public override void Touch( Entity other )
 	{
-
 		if ( !other.IsServer ) return;
 		if ( other is not PlatformerPawn pl ) return;
 
@@ -57,16 +58,12 @@ public partial class PlatformerDecay : ModelEntity
 
 		ColourChange( TimeToHold );
 
-		if ( TimeToHold / 10 >= DecayTime )
+		if ( TimeToHold / 10 >= DecayTime && !Broken )
 		{
-			EnableDrawing = false;
-			EnableAllCollisions = false;
-
-			RespawnPlat();
+			Break();
 		}
 
 		base.Touch( other );
-
 	}
 
 	[Event.Tick.Server]
@@ -75,22 +72,30 @@ public partial class PlatformerDecay : ModelEntity
 		RenderColor = Color.Lerp( DefaultColor, Color.Red, colorblend / 10 / Math.Max( DecayTime, .1f ) );
 	}
 
-	public override void EndTouch( Entity other )
+	private void Break()
 	{
-		base.EndTouch( other );
+		Broken = true;
+		EnableDrawing = false;
+		EnableAllCollisions = false;
 
-		if ( !other.IsServer ) return;
-		if ( other is not PlatformerPawn pl ) return;
-
-		RenderColor = DefaultColor;
-		TimeToHold = 0;
+		UnbreakAfter( RespawnTime );
 	}
 
-	public async void RespawnPlat()
+	private async void UnbreakAfter( float delay )
 	{
-		await GameTask.DelaySeconds( RespawnTime );
+		if ( !Broken )
+		{
+			Log.Warning( "Trying to respawn a platform that isn't decayed" );
+			return;
+		}
+
+		await GameTask.DelaySeconds( delay );
+
 		EnableDrawing = true;
 		EnableAllCollisions = true;
+		RenderColor = DefaultColor;
+		TimeToHold = 0;
+		Broken = false;
 	}
 
 
