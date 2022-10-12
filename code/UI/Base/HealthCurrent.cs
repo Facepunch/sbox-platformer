6a51377c7f0d42b76fafa7a5c4f07645af291e2b
bakscratch <lt_8@hotmail.co.uk>
2022-10-12 22:42:20 +0100

    Put Player Citizen in Hud
    https://files.facepunch.com/louie/1b1211b1/sbox-dev_Q0dDkppNYy.png

--- code/UI/Base/HealthCurrent.cs
+++ code/UI/Base/HealthCurrent.cs
@@ -7,13 +7,21 @@ namespace Platformer.UI
 {
 	public class HealthCurrent : Panel
 	{
-
+		static Color HighColor = new Color( 0.45f, 0.72f, 1f );
+		
+		static Color LowColor = new Color( 0.83f, 0.18f, 0.19f );
+			
 		private const int MaxHealth = 4;
 
 		public Image HealthImage;
-
+		public CitizenPanel Avatar { get; set; }
+			
 		public HealthCurrent()
 		{
+			
+			Avatar = new CitizenPanel( Local.Client );
+			AddChild( Avatar );
+			
 			for ( int i = 0; i < MaxHealth; i++ )
 			{
 				var block = new Panel( this, $"health-block health-block-{i}" );
@@ -33,6 +41,8 @@ namespace Platformer.UI
 				if ( block == null ) continue;
 
 				block.SetClass( "visible", pl.Health >= i + 1 );
+				
+				Avatar.Style.BackgroundColor = pl.Health >= i + 1 ? HighColor : LowColor;
 			}
 		}
 
