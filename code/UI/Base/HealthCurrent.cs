b122d78141d5edea1ed2eebf9a78730a6011c83a
Tony <devultj@gmail.com>
2022-10-13 10:17:27 -0400

    Clean up HealthCurrent, iterate through list of blocks instead of children

--- code/UI/Base/HealthCurrent.cs
+++ code/UI/Base/HealthCurrent.cs
@@ -1,32 +1,22 @@
 ﻿
 using Sandbox;
 using Sandbox.UI;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Platformer.UI
 {
 	public class HealthCurrent : Panel
 	{
-	//	static Color HighColor = new Color( 0.45f, 0.72f, 1f );
-		
-	//	static Color LowColor = new Color( 0.83f, 0.18f, 0.19f );
-			
 		private const int MaxHealth = 4;
-
 		public Image HealthImage;
-		//public CitizenPanel Avatar { get; set; }
-			
+
+		List<Panel> Blocks { get; set; } = new();
+
 		public HealthCurrent()
 		{
-			
-		//	Avatar = new CitizenPanel( Local.Client );
-		//	AddChild( Avatar );
-			
 			for ( int i = 0; i < MaxHealth; i++ )
-			{
-				var block = new Panel( this, $"health-block health-block-{i}" );
-			}
-
+				Blocks.Add( Add.Panel( $"health-block health-block-{i}" ) );
 		}
 
 		public override void Tick()
@@ -35,14 +25,13 @@ namespace Platformer.UI
 
 			if ( Local.Pawn is not Player pl ) return;
 
-			for( int i = 0; i < MaxHealth; i++ )
+			for ( int i = 0; i < Blocks.Count; i++ )
 			{
-				var block = Children.ElementAtOrDefault( i );
+				var block = Blocks[i];
 				if ( block == null ) continue;
+
 				block.SetClass( "visible", pl.Health >= i + 1 );
-							
 			}
-		//	Avatar.Style.BackgroundColor = pl.Health >= 1 ? HighColor : LowColor;
 		}
 
 	}
