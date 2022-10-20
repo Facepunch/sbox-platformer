2c21e7449895d32ed8dee2de61a71d563982227c
Jake Evans <jacob.ee92@gmail.com>
2022-10-19 18:26:56 -0600

    Tweaker volume is based on your distance from the bounding box surface

--- code/hammer/Sound/MusicBoxTweaker.cs
+++ code/hammer/Sound/MusicBoxTweaker.cs
@@ -1,7 +1,8 @@
-﻿using SandboxEditor;
+﻿
+using SandboxEditor;
 using Sandbox;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System;
 
 namespace Platformer;
 
@@ -23,72 +24,74 @@ partial class MusicBoxTweaker : ModelEntity
 	[Net]
 	public Vector3 Maxs { get; set; } = new Vector3( 32, 32, 64 );
 
-	public Sound PlayingSound { get; protected set; }
-
-	[Net]
-	private float speed { get; set; } = 10f;
+	private MusicBox MusicBox;
+	private Vector3[] DirectionLut = new Vector3[]
+	{
+		Vector3.Left,
+		Vector3.Right,
+		Vector3.Forward,
+		Vector3.Backward
+	};
 
 	public override void Spawn()
 	{
 		base.Spawn();
 
-		Tags.Add( "trigger" );
-		EnableAllCollisions = true;
-		EnableTouch = true;
-
-		var bounds = new BBox( Position + Mins, Position + Maxs );
-		var extents = (bounds.Maxs - bounds.Mins) * 0.5f;
-
-		var trigger = new BaseTrigger();
-		trigger.SetParent( this, null, Transform.Zero );
-		trigger.SetupPhysicsFromAABB( PhysicsMotionType.Static, Mins, Maxs );
-		trigger.Transmit = TransmitType.Always;
-		trigger.EnableTouchPersists = true;
-
 		Transmit = TransmitType.Always;
-
 	}
 
-	public override void Touch( Entity other )
+	[Event.Frame]
+	public void OnFrame()
 	{
-		base.Touch( other );
+		MusicBox ??= FindByName( TargetMusicBox ) as MusicBox;
+		if ( !MusicBox.IsValid() ) return;
 
-		if ( other is not PlatformerPawn pl ) return;
-
-		UpdateSound( To.Single( pl.Client ), true );
-	}
-
-	public override void EndTouch( Entity other )
-	{
-		base.EndTouch( other );
-
-
-		if ( other is not PlatformerPawn pl ) return;
-
-		UpdateSound( To.Single( pl.Client ), false );
-
-	}
-
-	[ClientRpc]
-	public void UpdateSound( bool Enter )
-	{
-		var target = FindByName( TargetMusicBox );
-		if ( target is not MusicBox ent ) return;
-
-		if ( Enter )
+		var pos = CurrentView.Position;
+		if ( Local.Pawn.IsValid() )
 		{
-			ent.UpdateVolume( 0.01f.LerpTo( 1, Time.Delta * speed ) );
-		}
-		else
-		{
-			ent.UpdateVolume( 1f.LerpTo( 0.01f, Time.Delta * speed ) );
+			pos = Local.Pawn.Position;
 		}
 
+		var bbox = new BBox( Position + Mins, Position + Maxs );
+		var playerBbox = new BBox( pos - new Vector3( 8, 8, 0 ), pos + new Vector3( 8, 8, 64 ) );
+
+		if ( !bbox.Overlaps( playerBbox ) )
+			return;
+
+		var dist = ShortestDistanceToSurface( bbox, pos ) - 8.0f;
+		var vol = Math.Max( dist.LerpInverse( 0f, 64f ), 0.1f );
+
+		if ( BasePlayerController.Debug )
+		{
+			DebugOverlay.Box( bbox, Color.Green );
+			DebugOverlay.Text( vol.ToString(), bbox.Center, 0, 3000 );
+		}
+
+		MusicBox.UpdateVolume( vol );
 	}
 
-	[Event.Tick.Client]
-	public void Tick()
+	private float ShortestDistanceToSurface( BBox bbox, Vector3 position )
 	{
+		var result = float.MaxValue;
+		var point = Vector3.Zero;
+		foreach ( var dir in DirectionLut )
+		{
+			var closetsPoint = bbox.ClosestPoint( position + dir * 10000 );
+			var dist = Vector3.DistanceBetween( closetsPoint, position.WithZ( closetsPoint.z ) );
+			if( dist < result )
+			{
+				point = closetsPoint;
+				result = dist;
+			}
+		}
 
+		if ( BasePlayerController.Debug )
+		{
+			DebugOverlay.Sphere( point, 3f, Color.Red, 0, false );
+			DebugOverlay.Line( point, position, 0f, false );
+		}
+
+		return result;
 	}
+
 }
